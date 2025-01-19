60f39b1e64c4cede4f96e43d793ac62fe1eaac7a
AinaWahida <ainaroshimi@gmail.com>
2025-01-20 04:21:24 +0800

    remove dao

--- src/main/java/com/myweb/dao/CategoryDAO.java
+++
@@ -1,9 +0,0 @@
-package com.myweb.dao;
-
-import com.myweb.model.Category;
-import java.util.List;
-
-public interface CategoryDAO {
-    List<Category> getAllCategories();
-    Category getCategoryById(int id);
-}
--- src/main/java/com/myweb/dao/UserDAO.java
+++
@@ -1,9 +0,0 @@
-package com.myweb.dao;
-
-import com.myweb.model.User;
-import java.util.List;
-
-public interface UserDAO {
-    User getUserByUsername(String username);
-    void addUser(User user);
-}
